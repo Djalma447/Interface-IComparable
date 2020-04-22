14c7465c5e3631ddea26a21dc06a3b91b8576289
Djalma <djalma.ant@gmail.com>
2020-04-22 02:01:31 -0300

    First Question solved

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Course
 {
@@ -6,7 +8,29 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\Test.txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach(string x in list)
+                    {
+                        Console.WriteLine(x);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
